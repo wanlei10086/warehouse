8cfa94b497e2eb90bcea68e6e36bb4a54a380081
Wan Lei <2926912559@qq.com>
2023-10-27 14:03:18 +0800

    one

---
+++ 1.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2023-10-27 10:47:48
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2023-10-27 10:48:51
+ * @FilePath: \智慧校园项目\npm包\test\index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+class 实验 {
+    constructor() {
+      this.name = "实验";
+    }
+    getName() {
+      return this.name;
+    }
+  }
